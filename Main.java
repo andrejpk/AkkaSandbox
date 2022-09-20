0f9d866147ca9dd7acb2829333eccc2319133a07
Andrej Kyselica <andrej@kyselica.com>
2022-09-20 20:50:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+import akka.Done;
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.util.ByteString;
+import akka.stream.*;
+import akka.stream.javadsl.*;
+
+import java.nio.file.Paths;
+import java.math.BigInteger;
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+class Main {
+  public static void main(String[] args) {
+    final ActorSystem system = ActorSystem.create("QuickStart");
+
+    final Source<Integer, NotUsed> source = Source.range(1, 100);
+
+    System.out.println("Hello world!");
+
+    final CompletionStage<Done> done = source.runForeach(i -> System.out.println(i), system);
+    done.thenRun(() -> system.terminate());
+  }
+}
